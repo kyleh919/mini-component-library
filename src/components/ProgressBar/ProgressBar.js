c91b243db8a385eaa319d41d3ede37836acac295
Kyle Hunter <kyle.hunter91@gmail.com>
2023-02-09 05:30:44 -0800

    Initial ProgressBar implementation

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,31 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const Status = styled.strong`
+  margin-right: 64px;
+`;
+
+const BarWrapper = styled.div`
+  display: inline-block;
+  background: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+
+  width: 370px;
+  height: 24px;
+`;
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <>
+      <Status>{value}</Status>
+      <BarWrapper></BarWrapper>
+    </>
+  );
 };
 
 export default ProgressBar;
