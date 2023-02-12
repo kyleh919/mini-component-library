726e41e645decedf4930601d95f2f3b72b7b77fa
Kyle Hunter <kyle.hunter91@gmail.com>
2023-02-12 11:25:16 -0800

    Base setup for Select Introduces Wrapper, NativeSelect, and PresentationalSelect

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
+
+const Wrapper = styled.div``;
+
+const NativeSelect = styled.select``;
+
+const PresentationalSelect = styled.div``;
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalSelect></PresentationalSelect>
+    </Wrapper>
   );
 };
 
