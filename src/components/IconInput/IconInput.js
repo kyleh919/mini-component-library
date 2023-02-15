1af8d7068131d2a89c11c2db4d4301eab9c2b602
Kyle Hunter <kyle.hunter91@gmail.com>
2023-02-14 20:20:07 -0800

    Applies custom styling for IconInput Applies IconWrapper and Wrapper to adjust Icon position

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,18 @@ import { COLORS } from "../../constants";
 import Icon from "../Icon";
 import VisuallyHidden from "../VisuallyHidden";
 
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 2px;
+  color: ${COLORS.gray700};
+`;
+
 const StyledInput = styled.input`
   color: ${COLORS.gray700};
   font-size: ${14 / 16}rem;
@@ -14,18 +26,29 @@ const StyledInput = styled.input`
   border: none;
   border-bottom: 2px solid black;
 
+  width: var(--width);
+
   &::placeholder {
     color: ${COLORS.gray500};
     font-weight: 400;
+
+    padding-left: 24px;
   }
 `;
 
 const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
   return (
-    <>
+    <Wrapper>
       <VisuallyHidden>{label}</VisuallyHidden>
-      <StyledInput type="text" placeholder={placeholder} />
-    </>
+      <IconWrapper>
+        <Icon id={icon} size={16} strokeWidth={size === "small" ? 1 : 2} />
+      </IconWrapper>
+      <StyledInput
+        type="text"
+        placeholder={placeholder}
+        style={{ "--width": width + "px" }}
+      />
+    </Wrapper>
   );
 };
 
