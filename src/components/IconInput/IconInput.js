95537638633d61a223157ea004076a3fe3129dde
Kyle Hunter <kyle.hunter91@gmail.com>
2023-02-14 20:43:38 -0800

    Applies conditional SIZES styling for StyledInput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -20,28 +20,36 @@ const IconWrapper = styled.div`
 
 const StyledInput = styled.input`
   color: ${COLORS.gray700};
-  font-size: ${14 / 16}rem;
+  font-size: var(--font-size);
   font-weight: 700;
+  line-height: var(--line-height);
 
   border: none;
   border-bottom: 2px solid black;
 
   width: var(--width);
+  padding-left: var(--padding-left);
 
   &::placeholder {
     color: ${COLORS.gray500};
     font-weight: 400;
-
-    padding-left: 24px;
   }
 `;
 
 const SIZES = {
   small: {
     iconStrokeWidth: 1,
+    iconSize: 16,
+    fontSize: `${14 / 16}rem`,
+    lineHeight: `${16 / 16}rem`,
+    inputPadding: 24 + "px",
   },
   large: {
     iconStrokeWidth: 2,
+    iconSize: 20,
+    fontSize: `${18 / 16}rem`,
+    lineHeight: `${21 / 16}rem`,
+    inputPadding: 32 + "px",
   },
 };
 
@@ -52,12 +60,21 @@ const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
     <Wrapper>
       <VisuallyHidden>{label}</VisuallyHidden>
       <IconWrapper>
-        <Icon id={icon} size={16} strokeWidth={styles.iconStrokeWidth} />
+        <Icon
+          id={icon}
+          size={styles.iconSize}
+          strokeWidth={styles.iconStrokeWidth}
+        />
       </IconWrapper>
       <StyledInput
         type="text"
         placeholder={placeholder}
-        style={{ "--width": width + "px" }}
+        style={{
+          "--width": width + "px",
+          "--line-height": styles.lineHeight,
+          "--font-size": styles.fontSize,
+          "--padding-left": styles.inputPadding,
+        }}
       />
     </Wrapper>
   );
