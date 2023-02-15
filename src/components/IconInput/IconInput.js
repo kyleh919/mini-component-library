085fb956fad17ad037d34c2e4ad021d028f0c908
Kyle Hunter <kyle.hunter91@gmail.com>
2023-02-14 20:02:49 -0800

    Initial StyledInput styling Supports custom ::placeholder styling Adjusts font and coloring Applies bottom-border

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,11 +6,25 @@ import { COLORS } from "../../constants";
 import Icon from "../Icon";
 import VisuallyHidden from "../VisuallyHidden";
 
+const StyledInput = styled.input`
+  color: ${COLORS.gray700};
+  font-size: ${14 / 16}rem;
+  font-weight: 700;
+
+  border: none;
+  border-bottom: 2px solid black;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+`;
+
 const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
   return (
     <>
       <VisuallyHidden>{label}</VisuallyHidden>
-      <input type="text" placeholder={placeholder} />
+      <StyledInput type="text" placeholder={placeholder} />
     </>
   );
 };
