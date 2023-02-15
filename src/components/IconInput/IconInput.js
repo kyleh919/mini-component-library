1a4065f872804e4d52f33802ac5794c57b41374b
Kyle Hunter <kyle.hunter91@gmail.com>
2023-02-14 19:41:14 -0800

    Base IconInput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  return (
+    <>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <input type="text" placeholder={placeholder} />
+    </>
+  );
 };
 
 export default IconInput;
